c8993354ce678fed6e63634fb5db962451fe3318
Piyush <thesourcepedia@gmail.com>
2022-09-13 15:27:21 +0530

    feat: flask api added for  - nlargest from heapq

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import heapq
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return {'message': 'Are you sure, whatever you are doing??'}
+
+
+@app.route('/nlargest', methods=['POST'])
+def nlargest():
+    data = request.json
+    summ_sent = heapq.nlargest(
+        data['size'], data['sent_scores'], key=data['sent_scores'].get)
+    return jsonify({'data': (''.join(summ_sent))})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
